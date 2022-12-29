0ae438507aae99df3b4cb70d83953092fa58209e
joh9911 <joh9911@naver.com>
2022-12-30 06:31:45 +0900

    test

--- app/src/main/java/com/example/youtube_transpose/Activity.kt
+++ app/src/main/java/com/example/youtube_transpose/Activity.kt
@@ -1,12 +1,76 @@
 package com.example.youtube_transpose
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.youtube_transpose.databinding.MainBinding
 
 class Activity: AppCompatActivity() {
+    var mBinding: MainBinding? = null
+    val binding get() = mBinding!!
+    lateinit var page: LinearLayout
+    lateinit var translateRight: Animation
+    lateinit var translateLeft: Animation
+
+    var isPageOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        mBinding = MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        page = binding.page
+        val toolbar = binding.toolBar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        translateRight = AnimationUtils.loadAnimation(this,R.anim.translate_right)
+        translateLeft = AnimationUtils.loadAnimation(this,R.anim.translate_left)
+        translateLeft.duration = 500
+        translateRight.duration = 500
+        val listener = object: Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {}
+            override fun onAnimationEnd(p0: Animation?) {
+                if (isPageOpen) {
+                    page.visibility = View.INVISIBLE
+
+                    isPageOpen = false
+                }
+                else{
+
+                    isPageOpen = true
+                }
+            }
+            override fun onAnimationRepeat(p0: Animation?) {}
+        }
+
+        translateRight.setAnimationListener(listener)
+        translateLeft.setAnimationListener(listener)
+        val button = binding.transposeBackButton
+        button.setOnClickListener {
+            page.startAnimation(translateRight)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item?.itemId){
+            R.id.transpose_icon -> {
+                    page.visibility = View.VISIBLE
+                    page.startAnimation(translateLeft)
+                    true
+                }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
