8203d69955ec5c877379c50a95e080c3c4130d8f
joh9911 <joh9911@naver.com>
2023-01-06 18:56:07 +0900

    refactor: 클릭 이벤트 구현

--- app/src/main/java/com/example/youtube_transpose/MelonRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/youtube_transpose/MelonRecyclerViewAdapter.kt
@@ -1,21 +1,27 @@
 package com.example.youtube_transpose
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.youtube_transpose.databinding.HomeRecyclerItemBinding
 
-class MelonRecyclerViewAdapter: RecyclerView.Adapter<MelonRecyclerViewAdapter.MyViewHolder>() {
-
-    var dataList = mutableListOf<MelonData>()
+class MelonRecyclerViewAdapter(dataList: MutableList<VideoData>): RecyclerView.Adapter<MelonRecyclerViewAdapter.MyViewHolder>() {
+    private val dataList = dataList
 
     inner class MyViewHolder(private val binding: HomeRecyclerItemBinding): RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(melonData: MelonData){
-            binding.rankingTextView.text = melonData.ranking
+        fun bind(melonData: VideoData){
             binding.accountTextView.text = melonData.account
             binding.titleTextView.text = melonData.title
-            binding.extraInfoTextView.text = melonData.extraInfo
+            Glide.with(binding.thumbnailImageView)
+                .load(melonData.thumbnail)
+                .into(binding.thumbnailImageView)
+
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -27,6 +33,18 @@ class MelonRecyclerViewAdapter: RecyclerView.Adapter<MelonRecyclerViewAdapter.My
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bind(dataList[position])
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
     }
-
+    // (2) 리스너 인터페이스
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+    // (4) setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener : OnItemClickListener
 }
