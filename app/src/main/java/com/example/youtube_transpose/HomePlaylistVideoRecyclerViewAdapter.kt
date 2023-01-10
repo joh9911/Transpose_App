9521abe097344d6f09444096aa817a592e723882
joh9911 <joh9911@naver.com>
2023-01-10 15:45:20 +0900

    feat: 인기 top 100 비디오 리사이클러뷰 생성

---
+++ app/src/main/java/com/example/youtube_transpose/HomePlaylistVideoRecyclerViewAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.youtube_transpose
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.youtube_transpose.databinding.HomePlaylistVideoRecyclerItemBinding
+
+class HomePlaylistVideoRecyclerViewAdapter(dataList: MutableList<VideoData>): RecyclerView.Adapter<HomePlaylistVideoRecyclerViewAdapter.MyViewHolder>() {
+    private val dataList = dataList
+
+    inner class MyViewHolder(private val binding: HomePlaylistVideoRecyclerItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(videoData: VideoData, position: Int){
+            binding.channelTextView.text = videoData.channel
+            binding.titleTextView.text = videoData.title
+            binding.rankingTextView.text = (position + 1).toString()
+            Glide.with(binding.thumbnailImageView)
+                .load(videoData.thumbnail)
+                .into(binding.thumbnailImageView)
+
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = HomePlaylistVideoRecyclerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(dataList[position], position)
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+    // (2) 리스너 인터페이스
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+    // (3) 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+    // (4) setItemClickListener로 설정한 함수 실행
+    private lateinit var itemClickListener : OnItemClickListener
+}
