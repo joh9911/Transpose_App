b807ee880081712ba9980a729c9b714171cde527
joh9911 <joh9911@naver.com>
2023-01-10 15:47:43 +0900

    feat: 레트로핏 객체 생성

--- app/src/main/java/com/example/youtube_transpose/RetrofitSuggestionKeyword.kt
+++ app/src/main/java/com/example/youtube_transpose/RetrofitSuggestionKeyword.kt
@@ -4,14 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitSuggestionKeyword {
+    var instance: Retrofit? = null
+    private const val BASE_URL = "https://suggestqueries.google.com/complete/"
     fun initRetrofit(): Retrofit {
-        if(RetrofitVideo.instance == null) { // 이 객체를 또 다른 클래스에서 쓰면 안되기 때문에 이렇게 narrowing을 해줌줌
-            RetrofitVideo.instance = Retrofit
+        if(instance == null) { // 이 객체를 또 다른 클래스에서 쓰면 안되기 때문에 이렇게 narrowing을 해줌줌
+            instance = Retrofit
                 .Builder()
-                .baseUrl("https://suggestqueries.google.com/complete/")//ip나 도메인: 포트번호
+                .baseUrl(BASE_URL)//ip나 도메인: 포트번호
                 .addConverterFactory(GsonConverterFactory.create()) //Gson을 쓰겠다.
                 .build()
         }
-        return RetrofitVideo.instance!!
+        return instance!!
     }
 }
--- app/src/main/java/com/example/youtube_transpose/RetrofitVideo.kt
+++ app/src/main/java/com/example/youtube_transpose/RetrofitVideo.kt
@@ -3,7 +3,7 @@ package com.example.youtube_transpose
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitClient {
+object RetrofitVideo {
     var instance: Retrofit? = null
     private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
     fun initRetrofit(): Retrofit{
@@ -18,14 +18,5 @@ object RetrofitClient {
         return instance!!
     }
 
-    fun searchSuggestionRetrofit(): Retrofit{
-        if(instance == null) { // 이 객체를 또 다른 클래스에서 쓰면 안되기 때문에 이렇게 narrowing을 해줌줌
-            instance = Retrofit
-                .Builder()
-                .baseUrl("https://suggestqueries.google.com/complete/")//ip나 도메인: 포트번호
-                .addConverterFactory(GsonConverterFactory.create()) //Gson을 쓰겠다.
-                .build()
-        }
-        return instance!!
-    }
+
 }
