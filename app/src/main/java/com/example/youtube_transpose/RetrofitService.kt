b68a11893f91323b675a8b7b0da4d791f887864b
joh9911 <joh9911@naver.com>
2023-01-10 15:47:12 +0900

    feat: 플레이리스트목록, 플레이리스트 정보를 위한 데이터클래스, 쿼리 함수 작성

--- app/src/main/java/com/example/youtube_transpose/RetrofitService.kt
+++ app/src/main/java/com/example/youtube_transpose/RetrofitService.kt
@@ -13,103 +13,246 @@ interface RetrofitService {
                         @Query("q") q: String,
                         @Query("maxResults") maxResult: String,
                         @Query("type") type: String
-    ): Call<YoutubeSearchData>
+    ): Call<VideoSearchData>
 
     @GET("search")
     fun getSuggestionKeyword(@Query("client") client: String,
                              @Query("ds") ds: String,
                              @Query("q") q: String
     ): Call<ResponseBody>
+
+    @GET("playlistItems")
+    fun getPlayListVideoItems(@Query("key") key: String,
+                         @Query("part") part: String,
+                         @Query("playlistId") playListId: String,
+                         @Query("pageToken") pageToken: String?
+    ): Call<PlayListVideoSearchData>
+
+    @GET("playlists")
+    fun getPlayLists(@Query("key") key: String,
+                     @Query("part") part: String,
+                     @Query("id") id: String // id는 각 플레이리스트의 아이디
+    ): Call<PlayListSearchData>
 }
 
-data class A(
-    val cookie: String
-)
-
-data class YoutubeSearchData (
-
+/**
+ * video Data class
+ */
+data class VideoSearchData (
     @SerializedName("kind"          ) var kind          : String?          = null,
     @SerializedName("etag"          ) var etag          : String?          = null,
     @SerializedName("nextPageToken" ) var nextPageToken : String?          = null,
     @SerializedName("regionCode"    ) var regionCode    : String?          = null,
-    @SerializedName("items"         ) var items         : ArrayList<Items> = arrayListOf()
-
+    @SerializedName("items"         ) var items         : ArrayList<VideoItems> = arrayListOf()
 )
-
-data class Items (
-
+data class VideoItems (
     @SerializedName("kind"    ) var kind    : String?  = null,
     @SerializedName("etag"    ) var etag    : String?  = null,
-    @SerializedName("id"      ) var id      : Id?      = Id(),
-    @SerializedName("snippet" ) var snippet : Snippet? = Snippet()
-
+    @SerializedName("id"      ) var id      : VideoId?      = VideoId(),
+    @SerializedName("snippet" ) var snippet : VideoSnippet? = VideoSnippet()
 )
-data class Snippet (
-
+data class VideoSnippet (
     @SerializedName("publishedAt"          ) var publishedAt          : String?     = null,
     @SerializedName("channelId"            ) var channelId            : String?     = null,
     @SerializedName("title"                ) var title                : String?     = null,
     @SerializedName("description"          ) var description          : String?     = null,
-    @SerializedName("thumbnails"           ) var thumbnails           : Thumbnails? = Thumbnails(),
+    @SerializedName("thumbnails"           ) var thumbnails           : VideoThumbnails? = VideoThumbnails(),
     @SerializedName("channelTitle"         ) var channelTitle         : String?     = null,
     @SerializedName("liveBroadcastContent" ) var liveBroadcastContent : String?     = null,
     @SerializedName("publishTime"          ) var publishTime          : String?     = null
+)
+data class VideoThumbnails (
+    @SerializedName("default" ) var default : VideoDefault? = VideoDefault(),
+    @SerializedName("medium"  ) var medium  : VideoMedium?  = VideoMedium(),
+    @SerializedName("high"    ) var high    : VideoHigh?    = VideoHigh()
+)
+data class VideoHigh (
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+)
+data class VideoMedium (
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+)
+data class VideoDefault (
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+)
+data class VideoId (
+    @SerializedName("kind"    ) var kind    : String? = null,
+    @SerializedName("videoId" ) var videoId : String? = null
+)
+
+data class PlayListVideoSearchData (
+
+    @SerializedName("kind"          ) var kind          : String?          = null,
+    @SerializedName("etag"          ) var etag          : String?          = null,
+    @SerializedName("nextPageToken" ) var nextPageToken : String?          = null,
+    @SerializedName("items"         ) var items         : ArrayList<PlayListVideoItems> = arrayListOf(),
+    @SerializedName("pageInfo"      ) var pageInfo      : PlayListVideoPageInfo?        = PlayListVideoPageInfo()
 
 )
-data class Thumbnails (
-
-    @SerializedName("default" ) var default : Default? = Default(),
-    @SerializedName("medium"  ) var medium  : Medium?  = Medium(),
-    @SerializedName("high"    ) var high    : High?    = High()
-
-)
-data class High (
+data class PlayListVideoDefault (
 
     @SerializedName("url"    ) var url    : String? = null,
     @SerializedName("width"  ) var width  : Int?    = null,
     @SerializedName("height" ) var height : Int?    = null
 
 )
-data class Medium (
+data class PlayListVideoMedium (
 
     @SerializedName("url"    ) var url    : String? = null,
     @SerializedName("width"  ) var width  : Int?    = null,
     @SerializedName("height" ) var height : Int?    = null
 
 )
-data class Default (
+data class PlayListVideoHigh (
 
     @SerializedName("url"    ) var url    : String? = null,
     @SerializedName("width"  ) var width  : Int?    = null,
     @SerializedName("height" ) var height : Int?    = null
 
 )
-data class Id (
+data class PlayListVideoStandard (
+
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+
+)
+data class PlayListVideoMaxres (
+
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+
+)
+data class PlayListVideoThumbnails (
+
+    @SerializedName("default"  ) var default  : PlayListVideoDefault?  = PlayListVideoDefault(),
+    @SerializedName("medium"   ) var medium   : PlayListVideoMedium?   = PlayListVideoMedium(),
+    @SerializedName("high"     ) var high     : PlayListVideoHigh?     = PlayListVideoHigh(),
+    @SerializedName("standard" ) var standard : PlayListVideoStandard? = PlayListVideoStandard(),
+    @SerializedName("maxres"   ) var maxres   : PlayListVideoMaxres?   = PlayListVideoMaxres()
+
+)
+data class PlayListVideoResourceId (
 
     @SerializedName("kind"    ) var kind    : String? = null,
     @SerializedName("videoId" ) var videoId : String? = null
+
+)
+data class PlayListVideoSnippet (
+
+    @SerializedName("publishedAt"            ) var publishedAt            : String?     = null,
+    @SerializedName("channelId"              ) var channelId              : String?     = null,
+    @SerializedName("title"                  ) var title                  : String?     = null,
+    @SerializedName("description"            ) var description            : String?     = null,
+    @SerializedName("thumbnails"             ) var thumbnails             : PlayListVideoThumbnails? = PlayListVideoThumbnails(),
+    @SerializedName("channelTitle"           ) var channelTitle           : String?     = null,
+    @SerializedName("playlistId"             ) var playlistId             : String?     = null,
+    @SerializedName("position"               ) var position               : Int?        = null,
+    @SerializedName("resourceId"             ) var resourceId             : PlayListVideoResourceId? = PlayListVideoResourceId(),
+    @SerializedName("videoOwnerChannelTitle" ) var videoOwnerChannelTitle : String?     = null,
+    @SerializedName("videoOwnerChannelId"    ) var videoOwnerChannelId    : String?     = null
+
+)
+data class PlayListVideoItems (
+
+    @SerializedName("kind"    ) var kind    : String?  = null,
+    @SerializedName("etag"    ) var etag    : String?  = null,
+    @SerializedName("id"      ) var id      : String?  = null,
+    @SerializedName("snippet" ) var snippet : PlayListVideoSnippet? = PlayListVideoSnippet()
+
+)
+data class PlayListVideoPageInfo (
+
+    @SerializedName("totalResults"   ) var totalResults   : Int? = null,
+    @SerializedName("resultsPerPage" ) var resultsPerPage : Int? = null
+
+)
+data class PlayListSearchData (
+
+    @SerializedName("kind"     ) var kind     : String?          = null,
+    @SerializedName("etag"     ) var etag     : String?          = null,
+    @SerializedName("pageInfo" ) var pageInfo : PlayListPageInfo?        = PlayListPageInfo(),
+    @SerializedName("items"    ) var items    : ArrayList<PlayListItems> = arrayListOf()
+
+)
+data class PlayListPageInfo (
+
+    @SerializedName("totalResults"   ) var totalResults   : Int? = null,
+    @SerializedName("resultsPerPage" ) var resultsPerPage : Int? = null
+
+)
+data class PlayListMedium (
+
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+
+)
+data class PlayListStandard (
+
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+
+)
+data class PlayListMaxres (
+
+    @SerializedName("url"    ) var url    : String? = null,
+    @SerializedName("width"  ) var width  : Int?    = null,
+    @SerializedName("height" ) var height : Int?    = null
+
+)
+data class PlayListThumbnails (
+
+    @SerializedName("medium"   ) var medium   : PlayListMedium?   = PlayListMedium(),
+    @SerializedName("standard" ) var standard : PlayListStandard? = PlayListStandard(),
+    @SerializedName("maxres"   ) var maxres   : PlayListMaxres?   = PlayListMaxres()
+
+)
+data class PlayListLocalized (
+
+    @SerializedName("title"       ) var title       : String? = null,
+    @SerializedName("description" ) var description : String? = null
+
+)
+data class PlayListSnippet (
+
+    @SerializedName("publishedAt"     ) var publishedAt     : String?     = null,
+    @SerializedName("channelId"       ) var channelId       : String?     = null,
+    @SerializedName("title"           ) var title           : String?     = null,
+    @SerializedName("description"     ) var description     : String?     = null,
+    @SerializedName("thumbnails"      ) var thumbnails      : PlayListThumbnails? = PlayListThumbnails(),
+    @SerializedName("channelTitle"    ) var channelTitle    : String?     = null,
+    @SerializedName("defaultLanguage" ) var defaultLanguage : String?     = null,
+    @SerializedName("localized"       ) var localized       : PlayListLocalized?  = PlayListLocalized()
+
+)
+data class PlayListItems (
+
+    @SerializedName("kind"    ) var kind    : String?  = null,
+    @SerializedName("etag"    ) var etag    : String?  = null,
+    @SerializedName("id"      ) var id      : String?  = null,
+    @SerializedName("snippet" ) var snippet : PlayListSnippet? = PlayListSnippet()
 
 )
 
 data class VideoData(
     val thumbnail: String,
     val title: String,
-    val account: String,
+    val channel: String,
     val videoId: String,
     val date: String,
 )
 
-//var vodid = if (kind == "youtube#video") {
-//    c.getJSONObject("id").getString("videoId") // 유튜브
-//} else {
-//    c.getJSONObject("id").getString("playlistId") // 유튜브
-//}
-//val title = c.getJSONObject("snippet").getString("title") //유튜브 제목을 받아옵니다
-//val ranking = (index + 1).toString()
-//val changString: String = stringToHtmlSign(title)
-//val date = c.getJSONObject("snippet").getString("publishedAt") //등록날짜
-//    .substring(0, 10)
-//val account = c.getJSONObject("snippet").getString("channelTitle")
-//val extraInfo = c.getJSONObject("snippet").getString("channelTitle")
-//val thumbnail = c.getJSONObject("snippet").getJSONObject("thumbnails")
-//    .getJSONObject("default").getString("url") //썸네일 이미지 URL값
+data class PlayListData(
+    val thumbnail: String,
+    val title: String,
+    val description: String
+)
