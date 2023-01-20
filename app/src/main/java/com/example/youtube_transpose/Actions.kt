06e05884d7a65a283e3effca298b135b8269e479
joh9911 <joh9911@naver.com>
2023-01-21 08:59:44 +0900

    feat: notification 을 위한 actions 추가

---
+++ app/src/main/java/com/example/youtube_transpose/Actions.kt
@@ -0,0 +1,14 @@
+package com.example.youtube_transpose
+
+
+object Actions {
+
+    private const val prefix = "com.gold24park.musicplayerexample.action."
+    const val MAIN = prefix + "main"
+    const val PREV = prefix + "prev"
+    const val NEXT = prefix + "next"
+    const val PLAY = prefix + "play"
+    const val START_FOREGROUND = prefix + "startforeground"
+    const val STOP_FOREGROUND = prefix + "stopforeground"
+
+}
