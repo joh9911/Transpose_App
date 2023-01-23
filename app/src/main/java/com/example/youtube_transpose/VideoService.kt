e36a184e875620705555ae8f380719a8d90cb82c
joh9911 <joh9911@naver.com>
2023-01-23 23:31:19 +0900

    feat: exoPlayer pause 시 알림 삭제 기능 구현

--- app/src/main/java/com/example/youtube_transpose/VideoService.kt
+++ app/src/main/java/com/example/youtube_transpose/VideoService.kt
@@ -3,6 +3,7 @@ package com.example.youtube_transpose
 import android.app.*
 import android.content.ContentValues
 import android.content.Intent
+import android.content.ServiceConnection
 import android.net.Uri
 import android.os.Binder
 import android.os.Build
@@ -11,6 +12,8 @@ import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackParameters
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
@@ -22,6 +25,7 @@ import com.yausername.youtubedl_android.YoutubeDLException
 class VideoService: Service() {
     lateinit var exoPlayer: SimpleExoPlayer
     lateinit var videoDetailData: VideoData
+    lateinit var activity: Activity
 
     companion object {
         const val TAG = "[MusicPlayerService]"
@@ -32,8 +36,18 @@ class VideoService: Service() {
 
     override fun onBind(p0: Intent?): IBinder? {
         exoPlayer.playWhenReady = true
-//        displayNotification()
         return VideoServiceBinder()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("비디오서비스","온디스트로이")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d("서비스","언바인드")
+        return super.onUnbind(intent)
+
     }
 
     override fun onCreate() {
@@ -44,15 +58,25 @@ class VideoService: Service() {
         exoPlayer = SimpleExoPlayer.Builder(this)
             .setTrackSelector(trackSelector)
             .build()
-    }
-    fun init(){
-        val trackSelector = DefaultTrackSelector(this).apply {
-            setParameters(buildUponParameters().setMaxVideoSizeSd())
-        }
-        exoPlayer = SimpleExoPlayer.Builder(this)
-            .setTrackSelector(trackSelector)
-            .build()
-        exoPlayer.playWhenReady = true
+        exoPlayer.addListener(object: Player.Listener{
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                when (playbackState){
+                    Player.STATE_READY -> {
+                        startForegroundService()
+                        if (!exoPlayer.isPlaying)
+                            stopForegroundService()
+                    }
+                    Player.STATE_ENDED -> {
+                    }
+                    Player.STATE_BUFFERING ->{
+                        //your logic
+                    }
+                    Player.STATE_IDLE -> {
+                    }
+
+                }
+            }
+        })
     }
 
     inner class VideoServiceBinder: Binder(){
@@ -82,72 +106,24 @@ class VideoService: Service() {
             return START_STICKY
 
     }
-    fun createNotification(): Notification {
-        // 알림 클릭시 MainActivity로 이동됨
-//        val notificationIntent = Intent(this, Activity::class.java)
-//        notificationIntent.action = Actions.MAIN
-//        notificationIntent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-//
-//
-//        val pendingIntent = PendingIntent
-//            .getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
 
-        // 각 버튼들에 관한 Intent
-        val prevIntent = Intent(this, VideoService::class.java)
-        prevIntent.action = Actions.PREV
-        val prevPendingIntent = PendingIntent
-            .getService(this, 0, prevIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        val playIntent = Intent(this, VideoService::class.java)
-        playIntent.action = Actions.PLAY
-        val playPendingIntent = PendingIntent
-            .getService(this, 0, playIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        val nextIntent = Intent(this, VideoService::class.java)
-        nextIntent.action = Actions.NEXT
-        val nextPendingIntent = PendingIntent
-            .getService(this, 0, nextIntent, PendingIntent.FLAG_IMMUTABLE)
-
-        // 알림
-        val notification = NotificationCompat.Builder(this, MusicNotification.CHANNEL_ID)
-            .setContentTitle("Youtube_Transpose")
-            .setContentText(videoDetailData.title)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setOngoing(true) // true 일경우 알림 리스트에서 클릭하거나 좌우로 드래그해도 사라지지 않음
-            .addAction(NotificationCompat.Action(android.R.drawable.ic_media_previous,
-                "Prev", prevPendingIntent))
-            .addAction(NotificationCompat.Action(android.R.drawable.ic_media_play,
-                "Play", playPendingIntent))
-            .addAction(NotificationCompat.Action(android.R.drawable.ic_media_next,
-                "Next", nextPendingIntent))
-//            .setContentIntent(pendingIntent)
-            .build()
-
-
-        // Oreo 부터는 Notification Channel을 만들어야 함
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val serviceChannel = NotificationChannel(
-                MusicNotification.CHANNEL_ID,
-                "Music Player Channel", // 채널표시명
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            val manager = this.getSystemService(NotificationManager::class.java)
-            manager?.createNotificationChannel(serviceChannel)
-        }
-        return notification
-    }
     private fun startForegroundService() {
-
-        startForeground(NOTIFICATION_ID, createNotification())
+        val notification = MusicNotification.createNotification(this, this)
+        startForeground(NOTIFICATION_ID, notification)
     }
 
     private fun stopForegroundService() {
-        stopForeground(true)
+
+        stopForeground(false)
         stopSelf()
     }
 
-    fun getVideoData(videoData: VideoData){
+    fun saveVideoData(videoData: VideoData){
         videoDetailData = videoData
+    }
+
+    fun initActivity(param: Activity) {
+        activity = param
     }
 
     fun setupVideoView(videoUrl: String){
@@ -166,33 +142,23 @@ class VideoService: Service() {
             .createMediaSource(MediaItem.fromUri(videoUrl))
         exoPlayer?.setMediaSource(videoSource)
         exoPlayer?.prepare()
-        startForegroundService()
-//        videoView.setVideoURI(Uri.parse(videoUrl))
+        setTempo(activity.tempoSeekBar.progress)
+        setPitch(activity.pitchSeekBar.progress)
+        exoPlayer?.play()
     }
-    private fun displayNotification() {
-        //Lets create our remote view.
-        val remoteView = RemoteViews(packageName, R.layout.video_notification)
 
-        //Next create a pending intent and make it stop our video playback.
-        val intent = PendingIntent.getService(this, 0, Intent(this, VideoService::class.java).apply {
-            putExtra(PLAY_PAUSE_ACTION, 0)
-        }, PendingIntent.FLAG_IMMUTABLE)
-        remoteView.setOnClickPendingIntent(R.id.stop_player_btn, intent)
-
-        //Now for showing through the notification manager.
-        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val notificationBuilder = NotificationCompat.Builder(this, "Default")
-
-        notificationBuilder.setContent(remoteView)
-        notificationBuilder.setSmallIcon(android.R.drawable.sym_def_app_icon)
-
-        //Check for version and create a channel if needed.
-        if (Build.VERSION.SDK_INT > 26) {
-            manager.createNotificationChannel(NotificationChannel("ID", "Main", NotificationManager.IMPORTANCE_DEFAULT))
-            notificationBuilder.setChannelId("ID")
-        }
-        val notification = notificationBuilder.build()
-        startForeground(0, notification)
-        manager.notify(NOTIFICATION_ID, notification)
+    fun setPitch(value: Int){
+        val pitchValue = value*0.05.toFloat()
+        val tempoValue = activity.tempoSeekBar.progress*0.05.toFloat()
+        val param = PlaybackParameters(1f + tempoValue, 1f + pitchValue)
+        exoPlayer?.playbackParameters = param
     }
+
+    fun setTempo(value: Int){
+        val tempoValue = value*0.05.toFloat()
+        val pitchValue = activity.pitchSeekBar.progress*0.05.toFloat()
+        val param = PlaybackParameters(1f + tempoValue, 1f + pitchValue)
+        exoPlayer?.playbackParameters = param
+    }
+
 }
