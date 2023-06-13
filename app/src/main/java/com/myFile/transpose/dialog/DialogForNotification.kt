6587c714614d99cb42557584c4e7cfa9a529c38f
joh9911 <joh9911@naver.com>
2023-06-13 19:47:10 +0900

    feat: 공지 다이알로그 생성

---
+++ app/src/main/java/com/myFile/transpose/dialog/DialogForNotification.kt
@@ -0,0 +1,39 @@
+package com.myFile.transpose.dialog
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.myFile.transpose.R
+
+class DialogForNotification: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            // Get the layout inflater
+            val inflater = requireActivity().layoutInflater;
+            val dialogView = inflater.inflate(R.layout.dialog_notification, null)
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder
+                .setView(dialogView)
+                // Add action buttons
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (dialog != null){
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog?.window?.setLayout(1000,1300)
+        }
+
+
+    }
+}
