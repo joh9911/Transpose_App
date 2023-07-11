d991addde46b1a459e11e1b697e2f5e542719bc6
joh9911 <joh9911@naver.com>
2023-07-11 14:51:07 +0900

    refactor: 밀리초 단위로 수정

---
+++ app/src/main/java/com/myFile/transpose/constants/TimeTarget.kt
@@ -0,0 +1,7 @@
+package com.myFile.transpose.constants
+
+object TimeTarget {
+    const val REVIEW_TARGET_DURATION: Long = 14L * 24 * 60 * 60 * 1000
+    const val DATA_DELETE_TARGET_DURATION: Long = 30L * 24 * 60 * 60 * 1000
+
+}
