b11b36953c553ad3cc3929cc598c2b9fd52b6560
joh9911 <joh9911@naver.com>
2023-08-12 21:12:49 +0900

    refactor: 이름 수정

--- app/src/main/java/com/myFile/transpose/database/Converters.kt
+++ app/src/main/java/com/myFile/transpose/database/Converters.kt
@@ -2,16 +2,16 @@ package com.myFile.transpose.database
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
-import com.myFile.transpose.retrofit.VideoData
+import com.myFile.transpose.model.VideoDataModel
 
 class Converters {
     @TypeConverter
-    fun dataToJson(value: VideoData): String{
+    fun dataToJson(value: VideoDataModel): String{
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToData(value: String): VideoData {
-        return Gson().fromJson(value, VideoData::class.java)
+    fun jsonToData(value: String): VideoDataModel {
+        return Gson().fromJson(value, VideoDataModel::class.java)
     }
 }
