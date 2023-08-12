fec3bed6686861c6b1946a598a58a0e62b8e1a75
joh9911 <joh9911@naver.com>
2023-08-12 21:06:13 +0900

    feat: model 정리

---
+++ app/src/main/java/com/myFile/transpose/model/VideoDetailDataModel.kt
@@ -0,0 +1,8 @@
+package com.myFile.transpose.model
+
+data class VideoDetailDataModel(
+    val videoTitle: String,
+    val videoViewCount: String,
+    val videoTime: String,
+    val channelId: String
+)
